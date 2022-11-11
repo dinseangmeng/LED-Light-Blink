8d3c05372a932f79c388fd62d63f04e4135cb0b3
dinseangmeng <dinseangmeng@gmail.com>
2022-11-11 19:06:17 +0700

    Add something more

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
 function openWeb(){
     window.open("http://dinseangmeng.xyz/")
 }
+
+var wrapper=document.querySelector("#wrapper");
+
+const randomColor=()=>{
+    var string="#";
+    var HexChar=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+    for(let i=0;i<6 ;i++){
+        string+=HexChar[Math.round(Math.random()*HexChar.length -1 )];
+    }
+    return string;
+}
+
+const BallBounch=(leftPos)=>{
+    const ball=document.createElement("div");
+    ball.className="ball";
+    ball.style.left=`${leftPos}px`;
+    ball.style.backgroundColor=randomColor();
+    wrapper.appendChild(ball);
+    setTimeout(()=>{
+        wrapper.removeChild(ball);
+    },2000)
+
+}
+window.addEventListener("mousemove",(e)=>BallBounch(e.clientX))
